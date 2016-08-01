b5ed1f4d5913666b9262742df3fbf6b795c06bfd
Van Phan <VanPhan514@gmail.com>
2016-08-01 17:00:04 -0400

    Created two models that has index and show action

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+House.destroy_all
+Character.destroy_all
+
+houses = House.create([{ name: 'Arryn' }, { name: 'Baratheon' }, { name: 'Bolton' }, { name: 'Frey' }, { name: 'Greyjoy' }, { name: 'Lannister' }, { name: 'Martell' }, { name: 'Stark' }, { name: 'Targaryen' }, { name: 'Tully' }, { name: 'Tyrell' }])
+names = ["Tyrion", "Jaime", "Cersei", "Daenerys", "Jon", "Petyr", "Jorah", "Sansa", "Arya", "Theon", "Bran", "Sandor", "Joffrey", "Catelyn"]
+
+names.length.times do |i|
+	Character.create(name: names[i], img_url: 'http://www.pngall.com/wp-content/uploads/2016/05/Game-of-Thrones-Logo.png', house: houses[rand(houses.length) - 1])
+end
